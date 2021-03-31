4403f6302089f8da14473b36d02b5810d95d9795
kkk-333 <49086809+kkk-333@users.noreply.github.com>
2021-03-31 21:00:03 +0530

    Update CountryController.java

--- rest/springREST1/src/main/java/com/madhu/springREST/controllers/CountryController.java
+++ rest/springREST1/src/main/java/com/madhu/springREST/controllers/CountryController.java
@@ -1,4 +1,4 @@
-package com.madhu.springREST.controllers;
+package com.kalyan.springREST.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
